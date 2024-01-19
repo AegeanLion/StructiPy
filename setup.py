46f04a15f9f22c2ff636abaab34dcc726a056882
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2024-01-19 14:18:39 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='StructiPy',
+	version='1.0.0',
+	url='https://github.com/AegeanLion/StructiPy.git',
+	author='Erim Keresteci',
+	author_email='aegeanlion@gmail.com',
+	description='This package contains widely used data structures as well as some methods to traverse them. This library is made for convenience so that you don not need to program your own data structures from scratch.',
+	packages=find_packages(),
+	install_requires=None
+	)
