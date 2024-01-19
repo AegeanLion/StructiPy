7f7be4bbb70c81f3bfbc9143ec63341feaa95435
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2024-01-19 14:31:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
 	name='StructiPy',
@@ -7,6 +7,6 @@ setup(
 	author='Erim Keresteci',
 	author_email='aegeanlion@gmail.com',
 	description='This package contains widely used data structures as well as some methods to traverse them. This library is made for convenience so that you don not need to program your own data structures from scratch.',
-	packages=find_packages('LinkedList'),
+	packages=['LinkedList']
 	install_requires=None
 	)
