bd86cfcd1c3365188d405c6b2a6f147b45287e53
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2024-01-22 00:17:04 -0500

    message

--- Stack.py
+++ Stack.py
@@ -1,20 +1,47 @@
 class Stack:
     def __init__(self, max_size=None):
-        self.top = None
+        self.top = self.get_top()
         self.max = max_size
         self.stack = []
 
-    def append(self):
-        return 
+    def append(self, val):
+        if self.max == None:
+            self.stack.append(val)
+        else:
+            if len(self.stack) == self.max:
+                return None
+            else:
+                self.stack.append(val)
+                return None
     
     def pop(self):
-        return 
-    
+        if len(self.stack) != 0:
+            return self.stack.pop()
+        else:
+            return None
+        
     def get_top(self):
-        return 
+        for i in self.stack:
+            top = i
+        return top
     
-    def display(self):
-        return 
+    def print(self):
+        print(self.stack) 
     
     def length(self):
-        return
+        return f"{len(self.stack)}/{self.max}"
+    
+def stackify(_list, max_size=None):
+    if max_size==None:
+        s = Stack()
+        s.stack = _list
+        return s
+    else:
+        s = Stack(max_size)
+        
+        if len(_list) <= max_size:
+            s.stack = _list
+        else:
+            s.stack = _list[:max_size]
+        
+        return s
