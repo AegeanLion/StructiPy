b01902b7ad35c39edccb2e728c3bfd5ca0150ce5
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2024-01-30 15:27:33 -0500

    Update BinaryTree.py
    added insert function to the object

--- BinaryTree.py
+++ BinaryTree.py
@@ -4,7 +4,6 @@ class BinaryNode:
         self.left = None
         self.right = None
 
-
 class BinaryTree:
     def __init__(self):
         self.root = None
@@ -15,10 +14,24 @@ class BinaryTree:
     def get_node(self):
         return 
     
-    def append(self):
-        return
-    
-    def pop(self):
+    def insert(self, val=None, current_node=self.root):
+        appended_node = BinaryNode(val)
+        
+        if appended_node.val:
+            if current_node == None or current_node.val == None:
+                current_node = appended_node:
+                return
+            elif appended_node.val > current_node.val:
+                if current_node.right == None:
+                    current_node.right = appended_node
+                else:
+                    self.insert(val, current_node.right)
+                    
+            elif appended_node.val < current_node.val:
+                if current_node.left == None:
+                    current_node.left = appended_node
+                else:
+                    self.insert(val, current_node.left)
         return 
     
     def size(self):
@@ -34,3 +47,8 @@ class BinaryTree:
         return 
 
     
+def bfs():
+    return
+
+def dfs():
+    return
