816ea9c4dee04e14654c18ff3a4927a6b478e6bc
AegeanLion <36489716+AegeanLion@users.noreply.github.com>
2024-01-30 12:31:12 -0500

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -4,19 +4,41 @@ class Queue:
         self.queue = []
         
     def push(self):
-        return 
+        if len(self.queue) != 0:
+            return self.queue.push()
+        else:
+            return None
     
-    def append(self):
-        return 
+    def append(self, val):
+        if self.max == None:
+            self.queue.append(val)
+        else:
+            if len(self.queue) == self.max:
+                return None
+            else:
+                self.queue.append(val)
+                return None
+
+    def force_append(self, val):
+        if self.max == None:
+            self.queue.append(val)
+        else:
+            if len(self.queue) == self.max:
+                self.push()
+                self.queue.append(val)
+                return None
+            else:
+                self.queue.append(val)
+                return None
     
     def length(self):
-        return
+        return f"{len(self.stack)}/{self.max}"
     
-    def first(self):
-        return 
+    def head(self):
+        return self.queue[0]
     
-    def last(self):
-        return
+    def tail(self):
+        return self.queue[-1]
     
-    def display(self):
-        print()
+    def print(self):
+        print(self.queue)
